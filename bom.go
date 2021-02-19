12a823906eb45328e8a3c105078cd0e9b43e4a46
Daeda1oss <52022128+daeda1oss@users.noreply.github.com>
2021-02-19 18:59:35 +0800

    Update bom.go

--- bom.go
+++ bom.go
@@ -1,9 +1,13 @@
 package csvreader
 
+import (
+	"bytes"
+)
+
 var BOM_UTF8 = []byte{239, 187, 191}
 
-func bomCheck(data []]byte) []byte{
-	if bytes.Equal(data[:3], BOM_UTF8){
+func bomCheck(data []byte) []byte {
+	if bytes.Equal(data[:3], BOM_UTF8) {
 		return data[3:]
 	}
 	return data
